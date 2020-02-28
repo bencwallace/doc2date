ac7e3bfc1afae154c4baff82c3ffe50125b18579
Ben <bencwallace@gmail.com>
2020-02-28 19:12:19 +0100

    fixed app text preprocessing

--- doc2date/app.py
+++ doc2date/app.py
@@ -1,8 +1,6 @@
 import pickle
 
-import numpy as np
-
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for
 
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import StandardScaler
@@ -11,8 +9,18 @@ from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
 
 # model components
+with open('pickle/tokenizer.pickle', 'rb') as f:
+    tokenizer = pickle.load(f)
+
+# convert SpaCy tokenizer to a function str -> list of str
+def tok(text):
+    return [str(t) for t in tokenizer(text)]
+
 with open('pickle/word2token.pickle', 'rb') as f:
     word2token = pickle.load(f)
+
+# build vectorizer from vocabulary and tokenizer
+vectorizer = CountVectorizer(vocabulary=word2token, tokenizer=tok)
 
 with open('pickle/transformer.pickle', 'rb') as f:
     transformer = pickle.load(f)
@@ -20,44 +28,35 @@ with open('pickle/transformer.pickle', 'rb') as f:
 with open('pickle/scaler.pickle', 'rb') as f:
     scaler = pickle.load(f)
 
-with open('pickle/model.pickle', 'rb') as f:
-    model = pickle.load(f)
+with open('pickle/predictor.pickle', 'rb') as f:
+    predictor = pickle.load(f)
 
 # app contents
 @app.route('/')
 def home():
-    return render_template('index.html')
+    with open('static/moby-dick.txt', 'r') as f:
+        text = f.read()
+    return render_template('index.html', text=text)
 
 @app.route('/predict', methods=['POST'])
 def predict():
     # get text
     text = request.form['text']
 
-    # tokenize
-    tokens = map(lambda w: word2token.get(w), text.split())
-    
-    # # count occurences
-    X = np.zeros(len(word2token))
-    print(X)
-    for token in tokens:
-        if token:
-            X[token] += 1
+    # count occurences
+    X = vectorizer.transform([text])
 
     # transform to tf-idf
-    X = transformer.transform(X.reshape(1, -1))
+    X = transformer.transform(X)
 
     # standardize
     X = scaler.transform(X.todense())
-    print(np.max(X))
 
     # predict
-    y = model.predict(X)
+    y = predictor.predict(X)
 
-    # form output
-    prediction = f'Predicted date: {int(y)}'
-
-    return render_template('index.html', prediction_text=prediction)
+    return render_template('index.html', prediction=int(y))
 
 # entry point
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
